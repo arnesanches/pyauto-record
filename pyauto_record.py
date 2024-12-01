9e9f1f18fb8ffb649477ab0d8a904f0fa7820426
arnesanches <arneantonio@hotmail.com>
2024-12-01 12:22:40 -0300

    Configuração inicial do projeto e importação de bibliotecas

---
+++ pyauto_record.py
@@ -0,0 +1,6 @@
+# Importando as bibliotecas necessárias
+import pyautogui  # Automação de ações no teclado e mouse
+import time  # Controle de tempo/pausas entre as ações
+
+# Configuração de uma pausa padrão entre os comandos do PyAutoGUI
+pyautogui.PAUSE = 0.5  # 0.5 segundos de intervalo entre os comandos
