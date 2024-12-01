f0a3bb65c4d428d9c2bbe6ca4f002320bd3f4755
arnesanches <arneantonio@hotmail.com>
2024-12-01 13:05:33 -0300

    Automação para abrir o navegador e acessar a URL

--- pyauto_record.py
+++ pyauto_record.py
@@ -4,3 +4,15 @@ import time  # Controle de tempo/pausas entre as ações
 
 # Configuração de uma pausa padrão entre os comandos do PyAutoGUI
 pyautogui.PAUSE = 0.5  # 0.5 segundos de intervalo entre os comandos
+
+# Abrindo o navegador Chrome
+pyautogui.press("win")  # Pressiona a tecla Windows para abrir o menu iniciar
+pyautogui.write("chrome")  # Digita "chrome" no menu iniciar
+pyautogui.press("enter")  # Pressiona "Enter" para abrir o navegador
+
+# Aguardando o navegador abrir completamente
+time.sleep(3)  # Pausa de 3 segundos para garantir que o navegador esteja pronto
+
+# Acessando a URL de login
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")  # Digita o link de login
+pyautogui.press("enter")  # Pressiona "Enter" para acessar a URL
